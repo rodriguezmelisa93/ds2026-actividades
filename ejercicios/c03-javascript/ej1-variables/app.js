8326ee8fbe874aa8d28593ff3d68cb8a9e270714
Melisa <rodriguezmelisa93@gmail.com>
2026-04-19 10:33:56 -0300

    1er-ejercicio-variables-terminado

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,16 @@
+
+
+const nombre = "Melisa Rodriguez";
+const edad = 32;
+const materia = "DESARROLLO DE SOFTWARE"; 
+
+console.log(`Hola me llamo ${nombre}, tengo ${edad} años y curso la materia ${materia}`);
+
+let contador = 0;
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log(` El valor final del contador es = ${contador} `);
+
+       
